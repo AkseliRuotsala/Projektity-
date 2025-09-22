7fa4501e94a4e2879072139a5f252d715b3999b3
akseliruotsala <akseli.ruotsala@metropolia.fi>
2025-09-22 10:21:15 +0300

    Muistion ja kokeilun lisäys

---
+++ kokeilu.py
@@ -0,0 +1,148 @@
+import random
+from collections import Counter
+
+suits = ['♠', '♥', '♦', '♣']
+ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+rank_values = {r: i for i, r in zip(range(2, 15), ranks)}
+
+# Kertoimet
+payouts = {
+    "Pari": 3,
+    "Kaksi paria": 4,
+    "Kolmoset": 5,
+    "Suora": 10,
+    "Väri": 12,
+    "Täyskäsi": 20,
+    "Neloset": 30,
+    "Värisuora": 50,
+}
+
+def new_deck():
+    deck = []
+    for s in suits:
+        for r in ranks:
+            deck.append((r, s))
+    random.shuffle(deck)
+    return deck
+
+def pretty(hand):
+    s = ''
+    for r, t in hand:
+        s += r + t + ' '
+    return s.strip()
+
+def choose_card(options):
+    print("Valitse kortti:")
+    i = 1
+    for c in options:
+        print(str(i) + ": " + c[0] + c[1])
+        i += 1
+    while True:
+        try:
+            val = int(input("Valinta (1 tai 2): "))
+            if val == 1 or val == 2:
+                return options[val-1]
+            else:
+                print("Anna 1 tai 2.")
+        except ValueError:
+            print("Anna numero 1 tai 2.")
+
+def hand_rank(cards):
+    """Palauttaa (arvo, nimi)"""
+    values = []
+    suits_in_hand = []
+    for r, s in cards:
+        values.append(rank_values[r])
+        suits_in_hand.append(s)
+    values.sort(reverse=True)
+    counts = Counter(values)
+    # Tehdään lista (määrä, arvo) ja järjestetään laskevaan
+    counts_items = []
+    for v, c in counts.items():
+        counts_items.append((c, v))
+    counts_items.sort(reverse=True)
+    is_flush = (len(set(suits_in_hand)) == 1)
+
+    # Straight detection
+    distinct_vals = sorted(list(set(values)), reverse=True)
+    is_straight = False
+    top_val = None
+    if len(distinct_vals) == 5:
+        if distinct_vals[0] - distinct_vals[4] == 4:
+            is_straight = True
+            top_val = distinct_vals[0]
+        if distinct_vals == [14,5,4,3,2]:
+            is_straight = True
+            top_val = 5
+
+    if is_straight and is_flush:
+        return (9, top_val), "Värisuora"
+    if counts_items[0][0] == 4:
+        return (8, counts_items[0][1]), "Neloset"
+    if counts_items[0][0] == 3 and counts_items[1][0] == 2:
+        return (7, counts_items[0][1]), "Täyskäsi"
+    if is_flush:
+        return (6,) + tuple(values), "Väri"
+    if is_straight:
+        return (5, top_val), "Suora"
+    if counts_items[0][0] == 3:
+        return (4, counts_items[0][1]), "Kolmoset"
+    if counts_items[0][0] == 2 and counts_items[1][0] == 2:
+        return (3, counts_items[0][1], counts_items[1][1]), "Kaksi paria"
+    if counts_items[0][0] == 2:
+        return (2, counts_items[0][1]), "Pari"
+    return (1,) + tuple(values), "Hai"
+
+def main():
+    saldo = 100  # aloitussaldo leikkirahaa
+    while True:
+        print("\nSinulla on saldoa:", saldo)
+        try:
+            panos = int(input("Anna panos (0 lopettaa): "))
+            if panos == 0:
+                print("Peli päättyy. Kiitos!")
+                break
+            if panos < 0 or panos > saldo:
+                print("Virheellinen panos.")
+                continue
+        except ValueError:
+            print("Anna numero.")
+            continue
+
+        deck = new_deck()
+        saldo -= panos
+
+        # Aloitus 2 kortilla
+        hand = [deck.pop(), deck.pop()]
+        print("Sinun aloituskätesi:", pretty(hand))
+
+        # Nostetaan 2 uutta, valitse toinen
+        print("\nNostetaan 2 uutta korttia...")
+        options = [deck.pop(), deck.pop()]
+        chosen = choose_card(options)
+        hand.append(chosen)
+        print("Valitsit:", chosen[0]+chosen[1])
+        print("Kätesi nyt:", pretty(hand))
+
+        # Lopuksi nostetaan 2 korttia automaattisesti
+        print("\nSaat vielä kaksi korttia automaattisesti...")
+        hand.append(deck.pop())
+        hand.append(deck.pop())
+
+        print("\n--- Lopullinen kätesi ---")
+        print(pretty(hand))
+
+        # Arvostellaan käsi
+        rank_tuple, name = hand_rank(hand)
+        print("Käden tyyppi:", name)
+
+        if name in payouts:
+            kerroin = payouts[name]
+            voitto = panos * kerroin
+            saldo += voitto
+            print("Voitit {} (kerroin {}x)!".format(voitto, kerroin))
+        else:
+            print("Ei voittoa tällä kertaa.")
+
+if __name__ == "__main__":
+    main()
